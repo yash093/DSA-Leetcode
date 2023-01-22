d1c755df2ec43dc7b23ec20094cdf991714fe95d
yashh0903 <114186487+yashh0903@users.noreply.github.com>
2023-01-22 23:32:43 +0530

    Time: 277 ms (7.72%), Space: 71.6 MB (5.00%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x:nums) mp[x]++;
+        for(auto x:mp) if(x.second > 1) return true;
+        return false;
+    }
+};
