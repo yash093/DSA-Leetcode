0fe8abacfba8ef55d8b31ab5c82f9bb749e1f707
yashh0903 <114186487+yashh0903@users.noreply.github.com>
2023-02-24 17:55:43 +0530

    Time: 199 ms (49.80%), Space: 109.5 MB (18.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,29 @@
 class Solution {
-vector<vector<int>> ans;
+    vector<vector<int>> ans;
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size(); i++){
-            if(i == 0 || nums[i-1] != nums[i]){
-                twoSum(i+1, nums.size()-1, nums, -nums[i]);
+            if(i == 0 || nums[i] != nums[i-1]){
+                twoSum(nums, i+1, nums.size() - 1, -nums[i]);
             }
         }
         return ans;
     }
-    void twoSum(int s, int e, vector<int> nums, int target){
-        int i = nums[s-1];
+    void twoSum(vector<int> nums, int s, int e, int target){
+        int a = nums[s-1];
         while(s < e){
             if(nums[s] + nums[e] == target){
-                ans.push_back({i,nums[s],nums[e]});
-                
+                ans.push_back({a,nums[s], nums[e]});
                 while(s < e && nums[s] == nums[s+1]) s++;
                 while(s < e && nums[e] == nums[e-1]) e--;
                 s++;
                 e--;
-            }else if(nums[s] + nums[e] > target) e--;
-             else s++;
+            }else if(nums[s] + nums[e] < target){
+                s++;
+            }else {
+                e--;
+            }
         }
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
